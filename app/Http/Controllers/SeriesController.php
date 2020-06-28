1ac4d68c4d3ccb9e3e6a45293711b001b58b07e3
Rony Anderson <ronypc@outlook.com>
2020-06-28 14:24:26 -0300

    criando post,get,put no lumen

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Providers\Serie;
+use Illuminate\Http\Request;
 
 class SeriesController
 {
     public function index()
     {
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json(Serie::create($request->all()), 201);
+    }
+
+    public function show(int $id)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)) {
+            return response()->json('', 204);
+        }
+        return response()->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)) {
+            return response()->json(['erro', 'Recurso não encontrado'], 404);
+        }
+        $serie->fill($request->all());
+        $serie->save();
+        return $serie;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Laravel\Lumen\Routing\Router;
 
 /*
@@ -18,6 +19,9 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => '/api'], function () use ($router) {
-    $router->get('/series', 'SeriesController@index');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('series', 'SeriesController@store');
+    $router->get('series', 'SeriesController@index');
+    $router->get('series/{id}', 'SeriesController@show');
+    $router->put('series/{id}', 'SeriesController@update');
 });
